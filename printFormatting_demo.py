f730badaa7998dbd151c1224cf12072471188300
Tanvir <tanvir0102@gmail.com>
2017-07-25 01:05:30 +0530

    demo print formatting with different examples

---
+++ printFormatting_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+print('This is a string')
+s = 'string'
+print('Place my variable here: %s' %s)
+x = 13.13
+print('Place my variable here: %s' %x)
+print('Floating point number: %1.1f' %(13.453))
+print('Floating point number: %1.3f' %(13.453))
+print('Floating point number: %5.3f' %(13.453))
+print('First: %s, Second: %s, Third: %s' %('Hi!!', 'two', 3))
+print('First: %s, Second: %s' %(2,2))
+print('First: {x}, Second: {y}, Third: {x}'.format(x='inserted', y='two!!'))
+

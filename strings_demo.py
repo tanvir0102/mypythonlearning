aba1599c9c56d700fab7da443e32ee734cf5401c
Tanvir <tanvir0102@gmail.com>
2017-07-25 00:45:33 +0530

    demo on string and some operation on string

---
+++ strings_demo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+str1 = 'Hello'
+str2 = 'This is also a string'
+str3 = "This is a string"
+str4 = "I'm a string"
+str5 = 'Hello World'
+
+print(str5)
+print('line 1 \n and \n line 2')
+
+print(str1)
+print(len(str1))
+print(str1[0])
+print(str1[1:])
+print(str1[:3])
+print(str1[:])
+print(str1[-1])
+print(str1[:-1])
+print(str1[::2])
+
+
+print('string are immutable, we can not change the elements')
+print(str5 + ' oncatenate me !')
+
+letter = 'z'
+print(letter*10)
+print(str1.upper())
+print(str1.lower())
+print(str2.split(' '))
+
+

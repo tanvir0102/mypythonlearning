63b97386aa0acb4a6549ebda1dc35db37822282e
Tanvir <tanvir0102@gmail.com>
2017-07-25 19:54:42 +0530

    demo on range and xrange

---
+++ range_demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+x = range(10)
+y = range(0,10)
+
+print(x)
+print(y)
+print(type(x))
+
+start = 5
+stop  = 20
+
+print(range(start, stop))
+print(range(start, stop, 2))
+
+l = [1,2,3,4,5]
+
+for num in range(1,12):
+	print(num)
+

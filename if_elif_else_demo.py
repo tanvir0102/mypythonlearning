2cdf5bb0fafd206f1196cd4066ed2986f6650a63
Tanvir <tanvir0102@gmail.com>
2017-07-25 12:12:22 +0530

    demo of the uses of if, elif, else statements

---
+++ if_elif_else_demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+if True:
+	print("Hello")
+
+x = True
+if x:
+	print('x was True')
+else:
+	print('I will print when x is anything not True')
+
+loc = 'Bank'
+if loc == 'Auto Shop':
+	print('Welcome to the Auto Shop')
+elif loc == 'Bank':
+	print('Welcome to the Bank')
+else:
+	print('Where are you?')
+

b8878a72d2fb5444b92234e50bee63277201f6d0
Tanvir <tanvir0102@gmail.com>
2017-07-25 10:45:34 +0530

    demo on tuple and operation, it's immutable

---
+++ tuples_demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env	python3
+
+#constructing tuples.
+t = (1,2,3)
+print(len(t))
+print(t[0])
+
+#basic tuple methods.
+print(t.index(2))
+print(t.count(1))
+t = (1,1,1,2,2,3,4)
+print(t.count(1))
+
+#Immutability.
+#we can't assigin values in tuple
+#When to use tuples.
